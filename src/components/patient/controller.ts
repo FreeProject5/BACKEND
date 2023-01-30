bd7efc8cd6a01bf81f3347626d23c8e083fa4202
HansUrpay <hansurpayh@gmail.com>
2023-01-30 14:08:31 -0500

    add controller create_users

--- src/components/patient/controller.ts
+++ src/components/patient/controller.ts
@@ -1,13 +1,27 @@
-import type { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import prisma from "../../datasource";
 import { success, failure } from "../../responses";
+import { hash_password, compare_password } from "../../utils/strings";
 
-export const getUser =async (_req:Request, res: Response):Promise<Response> => {
+export const get_ser =async (_req:Request, res: Response):Promise<Response> => {
     try {
         const user = await prisma.user.findMany()
     return success({res, data: user})
     } catch (error) {
         return failure({res, message:error})
+    }
+}
+
+export const create_user =async (req:Request, res: Response) => {
+    try {
+        const { body } = req;
+        body.password = hash_password(body.password)
+        body.last_session = new Date(body.last_session)
+        body.date_born = new Date(body.date_born)
+        const user = await prisma.user.create(body)
+        return success({res, status: 201, data: user})
+    } catch (error) {
+        return failure({res, message: error})
     }
 }
 
