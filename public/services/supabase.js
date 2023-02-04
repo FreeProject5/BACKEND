6c84443253a7bfbfd53a1ec9ebbb3b63019cdd0d
HansUrpay <hansurpayh@gmail.com>
2023-02-04 16:32:04 -0500

    tsc prueba deploy vercel

--- public/services/supabase.js
+++ public/services/supabase.js
@@ -1,22 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.post_patient = exports.get_patient = void 0;
+exports.post_patient = exports.get_patient = exports.supabase = void 0;
 const supabase_js_1 = require("@supabase/supabase-js");
 const dotenv_1 = require("dotenv");
 const responses_1 = require("../responses");
 (0, dotenv_1.config)();
-const supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_PROJECT_URL, process.env.SUPABASE_API_KEY);
+exports.supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_PROJECT_URL, process.env.SUPABASE_API_KEY);
 const get_patient = async (_req, res) => {
-    const posts = await supabase.from("Doctor").select("*");
-    return (0, responses_1.success)({ res, message: "All doctors", data: posts });
+    const posts = await exports.supabase.from("Patient").select("*");
+    return (0, responses_1.success)({ res, message: "All patient", data: posts });
 };
 exports.get_patient = get_patient;
 const post_patient = async (req, res) => {
     const { body } = req;
-    const data = await supabase.from("Doctor").insert(body).select();
+    const data = await exports.supabase.from("Patient").insert(body).select();
     return (0, responses_1.success)({
         res,
-        message: "Doctor created successfully",
+        message: "Patient created successfully",
         data: data.data
     });
 };
