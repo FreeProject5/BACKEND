882862a372c25ee4d0ebab3b0737a6fc91a0b809
yoroih <roycristhopherqp@gmail.com>
2023-02-03 21:10:34 -0500

    feat: add addspecialty

--- src/components/checkup/controller2.ts
+++ src/components/checkup/controller2.ts
@@ -18,3 +18,20 @@ export const add_doctor = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const add_specialty = async (req: Request, res: Response) => {
+    try {
+        const { body } = req;
+        const data = await supabase.from("Specialties").insert(body).select();
+        return success({
+            res,
+            message: "Specialty added succesfully",
+            data: data
+        });
+    } catch (error) {
+        return failure({
+            res,
+            message: error
+        });
+    }
+};
