810a9b7e6849d98d80775777f98b730851855075
brian24C <brianjosuecastro@hotmail.com>
2023-02-03 21:13:05 -0500

    add supabase para encontrar todos los  horarios de doctores

--- src/components/doctors/controller.ts
+++ src/components/doctors/controller.ts
@@ -100,9 +100,10 @@ export const deletee = async (req: Request, res: Response): Promise<Response>  =
 
 export const findAll_schedule = async (req: Request, res: Response): Promise<Response> => {
   try {
-    let Doctors = await prisma.schedule.findMany();
+    //let Doctors = await prisma.schedule.findMany();
+    const Schedule = await supabase.from("Schedule").select("*");
 
-    return success({ res, data: Doctors });
+    return success({ res, data: Schedule });
 
   } catch (error) {
     return failure({ res, message: error });
