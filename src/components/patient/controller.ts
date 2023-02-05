8e96a5f076c85869e552c375389748e576c65197
HansUrpay <hansurpayh@gmail.com>
2023-02-05 10:30:49 -0500

    create get_one_patient, update controllers patient

--- src/components/patient/controller.ts
+++ src/components/patient/controller.ts
@@ -2,11 +2,9 @@ import { Request, response, Response } from "express";
 import { supabase } from "../../services/supabase";
 import { success, failure } from "../../responses";
 import { hash_password, compare_password } from "../../utils/strings";
-import { generate_token, verify_token } from "../auth/auth";
+import { generate_token } from "../auth/auth";
 import { User } from "../interfaces";
 import { PostgrestResponse } from "@supabase/supabase-js";
-
-const patient = supabase.from("Patient");
 
 export const create_patient = async (
   req: Request,
@@ -20,7 +18,7 @@ export const create_patient = async (
       return failure({ res, message: "Username and password are required." });
     }
     body.password = hash_password(body.password);
-    const { data } = await patient.insert(body).select();
+    const { data } = await supabase.from("Patient").insert(body).select();
     return success({
       res,
       message: "User create successfully",
@@ -31,15 +29,36 @@ export const create_patient = async (
   }
 };
 
-export const get_patient = async (
+export const get_all_patient = async (
   _req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const { data } = await patient.select(
-      "id, firstname, lastname, phone, age, email, password"
-    );
+    const { data } = await supabase
+      .from("Patient")
+      .select("id, firstname, lastname, phone, age, email, password");
     return success({ res, data });
+  } catch (error) {
+    return failure({ res, message: error });
+  }
+};
+
+export const get_one_patient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const id = Number(req.params.id);
+    const data = await supabase
+      .from("Patient")
+      .select("id, firstname, lastname, phone, age, email, password")
+      .match({ id: id });
+    console.log(data);
+    if (data.data?.length === 0) {
+      return failure({ res, message: "Patient not exist" });
+    } else {
+      return success({ res, data: data.data });
+    }
   } catch (error) {
     return failure({ res, message: error });
   }
@@ -55,10 +74,15 @@ export const update_patient = async (
     if (body.password) {
       body.password = hash_password(body.password);
     }
-    const { data } = await patient.update({ data: body }).match({ id });
+    const datetime = new Date().toISOString();
+    body.update_at = datetime;
+    const { data } = await supabase
+      .from("Patient")
+      .update({ ...body })
+      .match({ id })
+      .select();
     return success({ res, message: "User updated successfully", data });
   } catch (error) {
-    console.log(error);
     return failure({ res, message: error });
   }
 };
@@ -69,41 +93,41 @@ export const delete_patient = async (
 ): Promise<Response> => {
   try {
     const id = Number(req.params.id);
-    const { data } = await patient.delete().match({ id });
+    const { data } = await supabase.from("Patient").delete().match({ id });
     return success({ res, message: "User deleted succesfully" });
   } catch (error) {
     return failure({ res, message: error });
   }
 };
 
-// export const login_patient = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {
-//   try {
-//     const { email, password } = req.body;
-//     const { data }: PostgrestResponse<User>  = await patient
-//       .select("id, firstname, lastname, phone, age, email, password")
-//       .match({ email });
-//     const {user} = data.data: PostgrestResponse<User>;
-//     if (!compare_password(user, password)) {
-//       return failure({ res, message: "Data does not exist or is incorrect" });
-//     } else {
-//       const datetime = new Date().toISOString();
-//       const last_session = await patient.update({
-//         where: { email },
-//         data: { last_session: datetime },
-//       });
-//       const token: string = generate_token(Number(user?.id));
-//       return success({
-//         res,
-//         message: `Welcome!`,
-//         data: user,
-//         token,
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return failure({ res, message: error });
-//   }
-// };
+export const login_patient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { email, password } = req.body;
+    const data: PostgrestResponse<User> = await supabase
+      .from("Patient")
+      .select("id, firstname, lastname, phone, age, email, password")
+      .match({ email });
+    if (data.data) {
+      compare_password(data.data[0].password, password);
+      const datetime = new Date().toISOString();
+      const last_session = await supabase
+        .from("Patient")
+        .update({ last_session: datetime })
+        .match({ email });
+      const token: string = generate_token(Number(data.data[0].id));
+      return success({
+        res,
+        message: `Welcome!`,
+        data: data.data,
+        token,
+      });
+    } else {
+      return failure({ res, message: "Data does not exist or is incorrect" });
+    }
+  } catch (error) {
+    return failure({ res, message: error });
+  }
+};
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Request, Response } from "express";
 import { config } from "dotenv";
-import { success} from "../responses";
 
 config();
 
@@ -10,17 +8,3 @@ export const supabase = createClient(
   process.env.SUPABASE_API_KEY as string
 );
 
-export const get_patient = async (_req: Request, res: Response) => {
-  const posts = await supabase.from("Patient").select("*");
-  return success({ res, message: "All patient", data: posts });
-};
-
-export const post_patient = async (req: Request, res: Response) => {
-  const { body } = req;
-  const data = await supabase.from("Patient").insert(body).select();
-  return success({
-    res,
-    message: "Patient created successfully",
-    data: data.data
-  });
-};
