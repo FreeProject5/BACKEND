e314449d8a20658635011a6dfb2640c5c75cf9a2
yoroih <roycristhopherqp@gmail.com>
2023-02-06 11:56:30 -0500

    chore: change routes

--- src/components/checkup/index.ts
+++ src/components/checkup/index.ts
@@ -6,8 +6,8 @@ const CheckupRouter: Router = Router();
 CheckupRouter.post("/", Controller.add_checkup);
 CheckupRouter.get("/", Controller.get_checkup);
 CheckupRouter.delete("/:id", Controller.delete_checkup);
-CheckupRouter.put("/update/:id", Controller.update_checkup);
-CheckupRouter.get("/bypatient/:id", Controller.getCheckup_byPatient);
+CheckupRouter.put("/:id", Controller.update_checkup);
+CheckupRouter.get("/:id", Controller.getCheckup_byPatient);
 CheckupRouter.get("/bydoctor/:id", Controller.getCheckup_byDoctor);
 
 export default CheckupRouter;
